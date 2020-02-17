bb856ff257b7af245d1caf21e28bc5ac84cfd38e
Peter Gorgone <peter@peltronic.com>
2020-02-17 12:17:36 -0800

    Replace GA with GTM

--- index.php
+++ index.php
@@ -24,6 +24,14 @@
     <link href="css/resume.min.css" rel="stylesheet">
     <link href="css/more-styles.css" rel="stylesheet">
 
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-WVJPZJ2');</script>
+    <!-- End Google Tag Manager -->
+
   </head>
 
   <body id="page-top">
@@ -512,17 +520,6 @@ include("./_modal.php");
     <!-- Custom scripts for this template -->
     <script src="js/resume.js"></script>
     <script src="js/utils.js"></script>
-
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-46190050-1', 'peterg-webdeveloper.com');
-  ga('send', 'pageview');
-</script>
-
   </body>
 
 </html>
