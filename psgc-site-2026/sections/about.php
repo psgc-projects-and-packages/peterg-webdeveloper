e548f045e8e1d7f504e95675f236c2874009b49c
Peter Gorgone <947820+peltronic@users.noreply.github.com>
2025-11-08 19:07:19 -0500

    updates

--- psgc-site-2026/sections/about.php
+++ psgc-site-2026/sections/about.php
@@ -5,16 +5,18 @@
                         Peter
                         <span class="text-primary">Gorgone</span>
                     </h1>
-                    <div class="subheading mb-5">
-                        3542 Berry Street · Cheyenne Wells, CO 80810 · (317) 585-8468 ·
-                        <a href="mailto:name@email.com">name@email.com</a>
+                    <h3 class="OFF-mb-5">Freelance Web Developer</h3>
+                    <div class="subheading mb-5">Las Vegas · Los Angeles · Chicago ·
+                        <a href="#" onclick="navigator.clipboard.writeText('peter@peltronic.com'); return false;" style="text-decoration: none; cursor: pointer;">peter@peltronic.com</a>
                     </div>
-                    <p class="lead mb-5">I am experienced in leveraging agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.</p>
+                    <p class="mb-5"> Professional Custom Web Development for Small Business and Startups</p>
                     <div class="social-icons">
-                        <a class="social-icon" href="#!"><i class="fab fa-linkedin-in"></i></a>
-                        <a class="social-icon" href="#!"><i class="fab fa-github"></i></a>
-                        <a class="social-icon" href="#!"><i class="fab fa-twitter"></i></a>
-                        <a class="social-icon" href="#!"><i class="fab fa-facebook-f"></i></a>
+                        <a class="social-icon" href="https://www.linkedin.com/in/petergorgone/" style="text-decoration: none;"><i class="fab fa-linkedin-in"></i></a>
+                        <a class="social-icon" href="https://github.com/peltronic" style="text-decoration: none;"><i class="fab fa-github"></i></a>
+                        <!--
+                        <a class="social-icon" href="https://twitter.com/peltronic" style="text-decoration: none;"><i class="fab fa-twitter"></i></a>
+                        <a class="social-icon" href="https://www.facebook.com/peltronic" style="text-decoration: none;"><i class="fab fa-facebook-f"></i></a>
+                        -->
                     </div>
                 </div>
             </section>
